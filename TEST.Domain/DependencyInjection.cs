8951db3d995cf870de233388dfee1d93f726d826
Chandan <chandan@inestweb.com>
2025-12-30 02:35:23 +0530

    Domain

---
+++ TEST.Domain/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TEST.Domain
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection InjectDomainDI(this IServiceCollection service)
+        {
+            return service;
+            // Configuration code here
+        }
+    }
+}
